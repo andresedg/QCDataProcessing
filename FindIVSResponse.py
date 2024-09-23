8a82844673f6e28df1b2e14ab8d63a92e1b4dca0
Andres Diaz <ae.digha@gmail.com>
2024-09-23 13:06:55 -0500

    File to process IVS data modified. Main menu added 2 different functions to process different sensor files, loop to main menu and loop to process another file created.

--- FindIVSResponse.py
+++ FindIVSResponse.py
@@ -3,65 +3,187 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from scipy.signal import find_peaks
 
-# Prompt the user to enter the file path
-filename = input("Please enter the full path of the file: ")
-print('\n')
-
-try:
-    # Open the file, read the first line (header), and remove the '/' if it exists
-    with open(filename, 'r') as file:
-        first_line = file.readline().strip()  # Read the first line (header)
-        if first_line.startswith('/'):
-            first_line = first_line[1:] 
-
-    # Attempt to read the file using Pandas
-    df = pd.read_table(filename, sep=r"\s+", header=0, skiprows=[0], names=first_line.split())
-    # If successful, display the first few rows
-    print("File loaded successfully! \n")
-
-except Exception as e:
-    # If there is any error, print an error message
-    print(f"Error: Unable to read the file. {e}")
-
-
-"""CLEANING AND FIXING FORMAT"""
-# Checking if 'DATE' and 'TIME' columns exist to proceed
-if 'DATE' in df.columns and 'TIME' in df.columns:
-    df['DATE'] = df['DATE'] + ' ' + df['TIME']
-    df['DATE'] = pd.to_datetime(df['DATE'], format='%m/%d/%y %H:%M:%S.%f')
-else:
-    print("Error: 'DATE' and/or 'TIME' columns not found.")
-    
-# Replacing empty spaces for NaN values
-df = df.replace(r'^\s*$', np.nan, regex=True)
-
-# Printing first lines of the data
-printOp = input("Enter 'Y' if you want to see the header of the data: \n").lower()
-
-if printOp == "y":
-    print(df.head())
+# Function to process each sensor of 5 coil system. Exproted from 'multi_sensor_data_processor.py' tool 
+def EM61_5coil_process_file():
+    # Prompt the user to enter the file path
+    filename = input("Please enter the full path of the file: ")
     print('\n')
 
-# Find peak indexes
-peaks_idx,_ = find_peaks(df['CH_1'],prominence=100)
-# Store peaks
-peaks = df.iloc[peaks_idx]
-# Reset rows index
-peaks.reset_index(drop=True, inplace=True)
+    try:
+        # Open the file, read the first line (header), and remove the '/' if it exists
+        with open(filename, 'r') as file:
+            first_line = file.readline().strip()  # Read the first line (header)
+            if first_line.startswith('/'):
+                first_line = first_line[1:] 
 
-printOp1 = input("Enter 'Y' if you want to see the peaks of the signal: \n").lower()
-if printOp == "y":
-    print(peaks[['CH_1', 'DATE']])
+        # Attempt to read the file using Pandas
+        df = pd.read_table(filename, sep=r"\s+", header=0, skiprows=[0], names=first_line.split())
+        # If successful, display the first few rows
+        print("File loaded successfully! \n")
 
-"""PLOTTING"""
+    except Exception as e:
+        # If there is any error, print an error message
+        print(f"Error: Unable to read the file. {e}")
 
-plotResponse = input("Enter 'Y' if you want to plot the data: \n").lower()
+    """CLEANING AND FIXING FORMAT"""
 
-if plotResponse == 'y':
-    print("Getting the plot... \n")
+    # Checking if 'DATE' and 'TIME' columns exist to proceed
+    if 'DATE' in df.columns and 'TIME' in df.columns:
+        df['DATETIME'] = df['DATE'] + ' ' + df['TIME']
+        df['DATETIME'] = pd.to_datetime(df['DATETIME'], format='%m/%d/%y %H:%M:%S.%f')
+    else:
+        print("Error: 'DATE' and/or 'TIME' columns not found.")
+        
+    # Replacing empty spaces for NaN values
+    df = df.replace(r'^\s*$', np.nan, regex=True)
 
-    plt.plot(df['DATE'],df['CH_1'],c='b')
-    plt.plot(peaks['DATE'],peaks['CH_1'],'o',c='r')
-    plt.title('IVS Plot')
-    plt.show()
-else:   print("Closing the program")
+    # Printing first lines of the data
+    printOp = input("Enter 'Y' if you want to see the header of the data: \n").lower()
+
+    if printOp == "y":
+        print(df.head())
+        print('\n')
+
+    # Find peak indexes
+    peaks_idx,_ = find_peaks(df['CH_1'],prominence=100)
+    # Store peaks
+    peaks = df.iloc[peaks_idx]
+    # Reset rows index
+    peaks.reset_index(drop=True, inplace=True)
+
+    printOp1 = input("Enter 'Y' if you want to see the peaks of the signal: \n").lower()
+    if printOp1 == "y":
+        print(peaks[['CH_1', 'DATETIME']])
+
+    """PLOTTING"""
+
+    plotResponse = input("Enter 'Y' if you want to plot the data: \n").lower()
+
+    if plotResponse == 'y':
+        print("Getting the plot... \n")
+
+        plt.figure(figsize=(10, 6))
+        plt.plot(df['DATETIME'],df['CH_1'],c='b')
+        plt.plot(peaks['DATETIME'],peaks['CH_1'],'o',c='r')
+        plt.title('IVS Plot')
+        plt.xticks(rotation=45)
+        plt.show()
+    else:   print("Closing the program")
+
+     # Ask if the user wants to work on another file
+    continue_choice = input("Would you like to process another sensor file or go to the menu? (Y/N): \n").lower()
+    
+    if continue_choice == 'y':
+        return True
+    else:
+        print("Exiting to the menu...\n")
+        return False
+
+# Function to process EM61 regular files. Imported directly from EM61 and converted to .xyz
+def EM61_process_file():
+    # Prompt the user to enter the file path
+    filename = input("Please enter the full path of the file: ")
+    print('\n')
+
+    columns = ['EAST', 'NORTH', 'STD-4-1', 'STD-4-2',
+               'STD-4-3', 'STD-4-4', 'GPS Correction', 'TIME']
+
+    try:
+        # Attempt to read the file using Pandas
+        df = pd.read_table(filename, sep=r"\s+",skiprows=2, names=columns, engine='python')
+        # If successful, display the first few rows
+        print("File loaded successfully! \n")
+
+    except Exception as e:
+        # If there is any error, print an error message
+        print(f"Error: Unable to read the file. {e}")
+
+     # Printing first lines of the data
+    printOp = input("Enter 'Y' if you want to see the header of the data: \n").lower()
+
+    if printOp == "y":
+        print(df.head())
+        print('\n')
+
+    """CLEANING AND FIXING FORMAT"""
+    # Convert the TIME column to timestamp format
+    df['TIME'] = pd.to_datetime(df['TIME'], format='%H:%M:%S.%f').dt.time   
+    try:
+        df['DATETIME'] = pd.to_datetime(df['TIME'], format='%H:%M:%S.%f', errors='coerce')
+    except ValueError:
+        df['DATETIME'] = pd.to_datetime(df['TIME'], format='%H:%M:%S', errors='coerce') 
+
+    # Replacing empty spaces for NaN values
+    df = df.replace(r'^\s*$', np.nan, regex=True)
+
+     # Find peak indexes
+    peaks_idx,_ = find_peaks(df['STD-4-2'],prominence=100)
+    # Store peaks
+    peaks = df.iloc[peaks_idx]
+    # Reset rows index
+    peaks.reset_index(drop=True, inplace=True)
+
+    printOp1 = input("Enter 'Y' if you want to see the peaks of the signal: \n").lower()
+    if printOp1 == "y":
+        print(peaks[['EAST', 'NORTH','STD-4-2', 'TIME']])
+
+    if peaks.empty:
+        print('There is no peaks to plot. Closing the programm... \n ')
+        plt.figure(figsize=(10, 6))
+        plt.plot(df['DATETIME'],df['STD-4-2'],c='b')
+        plt.title('STD-4-2 Data')
+        plt.xticks(rotation=45)
+        plt.show()
+        return False
+
+    """PLOTTING"""
+
+    plotResponse = input("Enter 'Y' if you want to plot the data: \n").lower()
+
+    if plotResponse == 'y':
+        print("Getting the plot... \n")
+
+        plt.figure(figsize=(10, 6))
+        plt.plot(df['DATETIME'],df['STD-4-2'],c='b')
+        plt.plot(peaks['DATETIME'],peaks['STD-4-2'],'o',c='r')
+        plt.title('IVS Plot: STD-4-2 Data')
+        plt.xticks(rotation=45)
+        plt.show()
+    else:   print("Closing the program")
+
+     # Ask if the user wants to work on another file
+    continue_choice = input("Would you like to process another sensor file or go to the menu? (Y/N): \n").lower()
+    
+    if continue_choice == 'y':
+        return True
+    else:
+        print("Exiting to the menu...\n")
+        return False
+
+# Main loop for menu
+def main_menu():
+    while True:
+        print("What type of file are you going to process?")
+        print("1) Single EM61 sensor file (from 5 Coil System)")
+        print("2) Regular EM61 file")
+        print("3) Exit")
+        menu_choice = input("Enter your option number or type 'Exit' to close the program:\n").lower()
+
+        if menu_choice == '1':
+            while True:
+                if not EM61_5coil_process_file():
+                    break
+        elif menu_choice == '2':
+            while True:
+                if not EM61_process_file():
+                    break
+        elif menu_choice == '3' or menu_choice == 'exit':
+            print('Closing the program...\n')
+            break
+        else:
+            print("Invalid selection. Please try again.\n")
+
+
+# Run the program
+if __name__ == "__main__":
+    main_menu()
